39eaf88bafce8fedd87051b3c17ffc94ff3c20a4
AliAberi <ali.aberi1995@gmail.com>
2023-04-25 12:20:41 +0430

    added recid model

---
+++ src/main/java/models/recid.java
@@ -0,0 +1,14 @@
+package models;
+
+public class recid {
+
+    private int Id;
+    private customer Customer;
+    private String Device;
+    private String DeviceAccessories;
+    private String IssueDate;
+    private String ExpirationDate;
+    private int Cost;
+    private String Description;
+
+}
