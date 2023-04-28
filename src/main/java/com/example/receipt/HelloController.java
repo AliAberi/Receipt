25f0409db9535bc011eb83bd6f3b0bc7c1aadad9
AliAberi <ali.aberi1995@gmail.com>
2023-04-28 23:08:23 +0430

    The action for the record and print button was set

--- src/main/java/com/example/receipt/HelloController.java
+++ src/main/java/com/example/receipt/HelloController.java
@@ -1,17 +1,31 @@
 package com.example.receipt;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void setAndPrint(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("DevLaunch Dialog");
+        alert.setHeaderText("An error has been encountered");
+        alert.setContentText("haalen");//from   www  .  ja va  2 s  .com
+        alert.showAndWait();
     }
+
+//    @FXML
+//    private Label welcomeText;
+
+
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
+
+
 
 
 }
